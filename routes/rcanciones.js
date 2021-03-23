1bead25a386a58942d681c1ba048baa90663342a
cuartasfabio <fabiocuartaspuente@gmail.com>
2021-03-23 20:06:42 +0100

    SDI-2021-505-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -26,4 +26,10 @@ module.exports = function(app) {
         res.send(respuesta);
     });
 
+    app.post('/cancion', function(req, res) {
+        res.send("Canción agregada: "+req.body.nombre+"<br>"
+            +" género: "+req.body.genero+"<br>"
+            +" precio: "+req.body.precio);
+    });
+
 };
