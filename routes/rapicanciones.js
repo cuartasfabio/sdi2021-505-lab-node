8b740b3c91dd63c034a911cba0fda6fb11ef10c4
cuartasfabio <fabiocuartaspuente@gmail.com>
2021-04-20 13:30:44 +0200

    SDI–2021-505-sesión10-Ejercicio-complementario1

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -33,26 +33,52 @@ module.exports = function(app, gestorBD) {
     app.delete("/api/cancion/:id", function(req, res) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
 
-        gestorBD.eliminarCancion(criterio,function(canciones){
-            if ( canciones == null ){
+        // Compruebo antes que sea eel autor de la cancion
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null || canciones[0].autor != res.usuario){
                 res.status(500);
                 res.json({
                     error : "se ha producido un error"
                 })
             } else {
-                res.status(200);
-                res.send( JSON.stringify(canciones) );
+
+                gestorBD.eliminarCancion(criterio,function(canciones){
+                    if ( canciones == null ){
+                        res.status(500);
+                        res.json({
+                            error : "se ha producido un error"
+                        })
+                    } else {
+                        res.status(200);
+                        res.send( JSON.stringify(canciones) );
+                    }
+                });
+
             }
         });
+
+
     });
 
     app.post("/api/cancion", function(req, res) {
-        let cancion = {
-            nombre : req.body.nombre,
-            genero : req.body.genero,
-            precio : req.body.precio,
+
+        let cancion = {}; // Comprobar presencia de atributos
+        if ( req.body.nombre != null)
+            cancion.nombre = req.body.nombre;
+        if ( req.body.genero != null)
+            cancion.genero = req.body.genero;
+        if ( req.body.precio != null)
+            cancion.precio = req.body.precio;
+        cancion.autor = res.usuario;
+
+
+        // Validar nombre, genero, precio
+        if( cancion.nombre.length > 5 || cancion.nombre.length > 30 || parseFloat(cancion.precio) < 0.0) {
+            res.status(500);
+            res.json({
+                error : "se ha producido un error: formato de datos incorrecto"
+            })
         }
-        // ¿Validar nombre, genero, precio?
 
         gestorBD.insertarCancion(cancion, function(id){
             if (id == null) {
@@ -75,27 +101,50 @@ module.exports = function(app, gestorBD) {
 
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
 
-        let cancion = {}; // Solo los atributos a modificar
-        if ( req.body.nombre != null)
-            cancion.nombre = req.body.nombre;
-        if ( req.body.genero != null)
-            cancion.genero = req.body.genero;
-        if ( req.body.precio != null)
-            cancion.precio = req.body.precio;
-        gestorBD.modificarCancion(criterio, cancion, function(result) {
-            if (result == null) {
+        // Compruebo antes que sea eel autor de la cancion
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null || canciones[0].autor != res.usuario){
                 res.status(500);
                 res.json({
                     error : "se ha producido un error"
                 })
             } else {
-                res.status(200);
-                res.json({
-                    mensaje : "canción modificada",
-                    _id : req.params.id
-                })
+
+                let cancion = {}; // Solo los atributos a modificar
+                if ( req.body.nombre != null)
+                    cancion.nombre = req.body.nombre;
+                if ( req.body.genero != null)
+                    cancion.genero = req.body.genero;
+                if ( req.body.precio != null)
+                    cancion.precio = req.body.precio;
+
+                // Validar nombre, genero, precio
+                if( cancion.nombre.length > 5 || cancion.nombre.length > 30 || parseFloat(cancion.precio) < 0.0) {
+                    res.status(500);
+                    res.json({
+                        error : "se ha producido un error: formato de datos incorrecto"
+                    })
+                }
+
+                gestorBD.modificarCancion(criterio, cancion, function(result) {
+                    if (result == null ) {
+                        res.status(500);
+                        res.json({
+                            error : "se ha producido un error"
+                        })
+                    } else {
+                        res.status(200);
+                        res.json({
+                            mensaje : "canción modificada",
+                            _id : req.params.id
+                        })
+                    }
+                });
+
             }
+
         });
+
     });
 
     app.post("/api/autenticar/", function(req, res) {
